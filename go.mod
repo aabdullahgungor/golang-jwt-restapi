383924257f9d802a93c6270d5299d0553906c36c
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-02 23:43:48 +0300

    user model updated

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/aabdullahgungor/golang-jwt-restapi
 go 1.19
 
 require (
+	golang.org/x/crypto v0.7.0
+	gorm.io/driver/mysql v1.4.7
+	gorm.io/gorm v1.23.8
+)
+
+require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/driver/mysql v1.4.7 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
--- model/user_model.go
+++ model/user_model.go
@@ -1,8 +1,11 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
-type User struct {
+type User struct { // User is for storing User details.
 	gorm.Model
 	Name     string `json:"name"`
 	Username string `json:"username" gorm:"unique"`
@@ -10,13 +13,29 @@ type User struct {
 	Password string `json:"password"`
 }
 
-type Authentication struct {
+type Authentication struct {  // Authentication is for login data.
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type Token struct {
+type Token struct {  // Token is for storing token information for correct login credentials.
 	Role        string `json:"role"`
 	Email       string `json:"email"`
 	TokenString string `json:"token"`
+}
+
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+func (user *User) CheckPassword(providedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
+	if err != nil {
+		return err
+	}
+	return nil
 }
