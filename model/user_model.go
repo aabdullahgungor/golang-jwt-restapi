7a1ff8c713997496a05e82148db4bc7b54968cdb
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-02 23:15:38 +0300

    model file added

---
+++ model/user_model.go
@@ -0,0 +1,22 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Username string `json:"username" gorm:"unique"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
+}
+
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Role        string `json:"role"`
+	Email       string `json:"email"`
+	TokenString string `json:"token"`
+}
