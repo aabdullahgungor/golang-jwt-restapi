6d5a5497e1c0044ef2f5218e8a8d9ed102b1390f
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-03 02:52:27 +0300

    main.go file updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"github.com/aabdullahgungor/golang-jwt-restapi/controller"
+	"github.com/aabdullahgungor/golang-jwt-restapi/database"
+	"github.com/aabdullahgungor/golang-jwt-restapi/middleware"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	// Initialize Database
+	database.Connect()
+	database.Migrate()
+	// Initialize Router
+	router := initRouter()
+	router.Run(":8080")
+
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/token", controller.GenerateToken)
+		api.POST("/user/register", controller.RegisterUser)
+		secured := api.Group("/secured").Use(middleware.Auth())
+		{
+			secured.GET("/ping", controller.Ping)
+		}
+	}
+	return router
 }
